65b55fa1ff6d1ed05a2cfc7802a95bffcd925cb0
Cristian CRC <cristiancrc97@hotmail.com>
2021-02-01 11:11:04 -0500

    Cliente Service added to get clients information from json file and show on view layer

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,15 @@
+import { Cliente } from './cliente';
+
+
+export const CLIENTES: Cliente[] = [
+  { id: 1, nombre: 'Cristian', apellido: 'Ramirez', createAt: '2021-01-29', email: 'cristian@correo' },
+  { id: 2, nombre: 'Camilo', apellido: 'Castellanos', createAt: '2020-06-29', email: 'camilo@correo' },
+  { id: 3, nombre: 'Santiago', apellido: 'Ramirez', createAt: '2004-06-13', email: 'santiago@correo' },
+  { id: 4, nombre: 'Gladis', apellido: 'Castellanos', createAt: '1975-20-03', email: 'gladis@correo' },
+  { id: 5, nombre: 'Leonardo', apellido: 'Castellanos', createAt: '1975-20-03', email: 'gladis@correo' },
+  { id: 6, nombre: 'Juan', apellido: 'Castellanos', createAt: '1975-20-03', email: 'gladis@correo' },
+  { id: 7, nombre: 'Andres', apellido: 'Castellanos', createAt: '1975-20-03', email: 'gladis@correo' },
+  { id: 8, nombre: 'Brian', apellido: 'Castellanos', createAt: '1975-20-03', email: 'gladis@correo' },
+  { id: 9, nombre: 'Alejandro', apellido: 'Castellanos', createAt: '1975-20-03', email: 'gladis@correo' },
+  { id: 10, nombre: 'Ivan', apellido: 'Castellanos', createAt: '1975-20-03', email: 'gladis@correo' },
+];
